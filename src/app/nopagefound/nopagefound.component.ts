277da7c8749649a49b459075a6e41a39dc141562
Pedro Daleth López López <pedrolpz51@gmail.com>
2022-07-22 22:46:00 -0500

    added routing for auth and pages

--- src/app/nopagefound/nopagefound.component.ts
+++ src/app/nopagefound/nopagefound.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styleUrls: ['../../../assets/css/pages/error-pages.css'],
+  styleUrls: ['../../assets/css/pages/error-pages.css'],
 })
 export class NopagefoundComponent {
   year: number = new Date().getFullYear();
